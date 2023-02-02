01531988e7fb1460472820b78d22c06fa8d91043
selimboulaaba <selimboulaaba@outlook.fr>
2023-02-02 02:09:34 +0100

    todo2

---
+++ todo2/src/Controller/StudentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController {
+
+    #[Route("/", name: "index")]
+    public function index() : Response {
+        return new Response("Bonjour mes étudiants");
+    }
+
+}
+
+?>
